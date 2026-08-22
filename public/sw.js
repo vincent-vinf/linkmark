13d9e6d2994e348b3cc142dbc415f9fae494efc2
shilongchen <857618786vin@gmail.com>
2026-08-22 14:49:10 +0800

    fix: activate PWA security updates reliably

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
-const CACHE = 'linkmark-static-v1';
-self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(['/']))));
+const CACHE = 'linkmark-static-v2';
+self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(['/'])).then(() => self.skipWaiting())));
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return;
-  event.respondWith(caches.match(event.request).then((cached) => cached ?? fetch(event.request).then((response) => { const copy = response.clone(); void caches.open(CACHE).then((cache) => cache.put(event.request, copy)); return response; })));
+  if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin || !['document', 'script', 'style', 'image', 'font', 'worker'].includes(event.request.destination)) return;
+  event.respondWith(fetch(event.request).then((response) => { const copy = response.clone(); void caches.open(CACHE).then((cache) => cache.put(event.request, copy)); return response; }).catch(() => caches.match(event.request).then((cached) => cached ?? caches.match('/'))));
 });
